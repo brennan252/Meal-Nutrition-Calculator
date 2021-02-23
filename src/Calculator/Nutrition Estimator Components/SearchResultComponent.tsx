5cb21a4cef966c299f960b59d285e77d9a1fce99
brennan252 <79476154+brennan252@users.noreply.github.com>
2021-02-23 14:35:41 -0800

    Add files via upload

---
+++ src/Calculator/Nutrition Estimator Components/SearchResultComponent.tsx
@@ -0,0 +1,113 @@
+// "SearchResultComponent.tsx"
+// Search Result component rendered in NameCheckInterface.tsx 
+// on mounting calls get(/names/).
+// If one match is returned, add the ID and name and unmount the component.
+// Otherwise, show multiple match display or no match display based on the match count
+
+import * as React from 'react';
+import { get } from 'jquery';
+import { Alert, Button, Spinner, Dropdown, DropdownButton, Form } from 'react-bootstrap';
+
+type Props = {
+    name: string;
+    index: number;
+
+    onResetName: any;
+    onSuggestionSelect: any;
+    onSuccessSuggestion: any;
+};
+
+
+export const SearchResultComponent = (Props: Props) => {
+    const [noMatch, setNoMatch] = React.useState(false);
+    const [multipleMatch, setMultipleMatch] = React.useState(false);
+    const [matchCount, setMatchCount] = React.useState(0 as number);
+    
+    const [matchNames, setMatchNames] = React.useState([]);
+    const [matchIDs, setMatchIDs] = React.useState([] as Array<number>);
+    const [isLoading, setIsLoading] = React.useState(true);
+
+
+    React.useEffect(() => {
+        if (Props.name) {
+            const urlString = "http://localhost:1337/names/".concat(Props.name);
+
+            get(urlString)
+                .then((result) => {
+                    setMatchCount(result.matchCount);
+                    setMatchNames(result.nameMatches);
+                    setMatchIDs(result.idMatches);
+
+                    let newMultipleMatch = false;
+                    let newNoMatch = false;
+
+                    // Multiple Matches
+                    if (result.matchCount > 1) {
+                        newMultipleMatch = true;
+                    }
+
+                    // One Match
+                    if (result.matchCount === 1) {
+                        Props.onSuccessSuggestion(Props.index, result.nameMatches[0], result.idMatches[0]);
+                    }
+
+                    // Zero Matches
+                    if (result.matchCount === 0) {
+                        newNoMatch = true;
+                    };
+
+                    setMultipleMatch(newMultipleMatch);
+                    setNoMatch(newNoMatch);
+                })
+                .then((res) => {
+                    setIsLoading(false);
+                });
+        }
+    }, []);
+
+    
+    if (isLoading) {
+        return (
+            <div>
+                <Form.Row className="justify-content-center" >
+                    <Spinner animation="border" variant="primary" />
+                </Form.Row>
+                <Form.Row className="justify-content-center" >
+                    <Button onClick={Props.onResetName} variant="danger">Cancel</Button>
+                </Form.Row>
+            </div>)
+    } else {
+        return (
+            <div>
+                {noMatch &&
+                    <Form.Row className="justify-content-center align-items-center" >
+                        <Alert variant="danger">No Matches Found for {Props.name} </Alert>
+                    </Form.Row>}
+                {noMatch &&
+                    <Form.Row className="justify-content-center align-items-center" >
+                         <Button onClick={Props.onResetName} variant="danger">Reset</Button>
+                    </Form.Row>}
+
+
+                {multipleMatch &&
+                    <Form.Row className="justify-content-center align-items-center" >
+                        <Alert variant="info">Multiple Matches Found for {Props.name} </Alert>
+                    </Form.Row>}
+                {multipleMatch &&
+                    <Form.Row className="justify-content-center align-items-center">
+                        <DropdownButton variant="secondary" text="light" title="Select A Name" >
+                            {matchNames.map((name) =>
+                                <Dropdown.Item key={Props.index.toString().concat('_', name)}
+                                    eventKey={Props.index.toString().concat('_', name, '_', matchIDs[matchNames.indexOf(name)].toString())}
+                                    onSelect={Props.onSuggestionSelect}>{name}</Dropdown.Item>)}
+                        </DropdownButton>
+                    </Form.Row>}
+                {multipleMatch &&
+                    <br />}
+                {multipleMatch &&
+                    <Form.Row className="justify-content-center align-items-center" >  
+                        <Button onClick={Props.onResetName} variant="danger">Reset</Button>
+                    </Form.Row>}
+            </div>)
+        }
+    };
